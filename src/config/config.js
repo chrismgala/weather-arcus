9141e7e3d574456535475fba48a7ee7c46e06107
Chris Gala <alagsirhc@gmail.com>
2017-09-18 13:27:13 -0700

    updated config.js

--- src/config/config.js
+++ src/config/config.js
@@ -1,11 +1,7 @@
 /* jshint node: true */
 module.exports = {
     PORT: 8080,
-
-	/* Our Historical Weather Data API */
-    DARK_SKY_API_KEY: "76babf1fd1074818ecf6c7191b77446b",
-    
+	
     /* Our backup for getting User location in case Geolocation is blocked or not supported */
     GOOGLE_MAPS_GEOCODING_URL: "https://maps.googleapis.com/maps/api/geocode/json",
-    GOOGLE_MAPS_GEOCODING_API_KEY: "AIzaSyAbAlCBDBs53bFPwHEz0wN_7upvlsuW3ZI"
 };
